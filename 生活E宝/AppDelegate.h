4b93ae10a288dcc150ead8f1a6036aa36d32c8d4
Admin <1033948449@qq.com>
2016-12-15 17:21:55 +0800

    Initial Commit

---
+++ 生活E宝/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  生活E宝
+//
+//  Created by Admin on 16/12/15.
+//  Copyright © 2016年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 生活E宝/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  生活E宝
+//
+//  Created by Admin on 16/12/15.
+//  Copyright © 2016年 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
